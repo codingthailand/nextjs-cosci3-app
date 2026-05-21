b65910020e217838c84d4dc64023eaac13e11384
Akenarin Komkoon <codingthaialnd@gmail.com>
2026-05-21 15:18:28 +0700

    update

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,8 +1,10 @@
 import FeaturesProduct from "@/components/features-product";
 import prisma from "@/lib/prisma";
+import { connection } from "next/server";
 
 // http://localhost:3000/product
 export default async function ProductPage() {
+  await connection(); // signals this is a dynamic route
   const products = await prisma.product.findMany();
 
   return (
