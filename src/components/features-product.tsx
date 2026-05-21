aff8e0b69b9c7112d0e6bcebfd4611ace7a9c79d
Akenarin Komkoon <codingthaialnd@gmail.com>
2026-05-21 15:50:53 +0700

    update

--- src/components/features-product.tsx
+++ src/components/features-product.tsx
@@ -1,4 +1,4 @@
-
+import Image from "next/image";
 
 type Props = {
   products: any[]
@@ -14,6 +14,19 @@ const FeaturesProduct = ({ products }: Props) => {
       <div className="mt-16 grid grid-cols-1 gap-6 sm:mt-20 sm:grid-cols-2 lg:grid-cols-3">
         {products.map((product) => (
           <div className="rounded-xl border bg-card px-6 py-7" key={product.id}>
+
+            <div className="relative mb-5 aspect-4/5 w-full overflow-hidden rounded-xl sm:mb-6">
+              <Image
+                alt={product.name}
+                className="size-full bg-muted object-cover"
+                width={0}
+                height={0}
+                sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
+                src={`/product-image/${product.picture}`}
+                loading="eager"
+              />
+            </div>
+
             <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-primary/5 text-primary dark:bg-primary/15">
               ID: {product.id}
             </div>
