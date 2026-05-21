1008a44596423623219a9a6bcb70cd8a31f503a7
Akenarin Komkoon <codingthaialnd@gmail.com>
2026-05-21 10:44:42 +0700

    update

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,9 +1,15 @@
 import FeaturesCourse from "@/components/features-course";
 
 // http://localhost:3000/course
-export default function CoursePage() {
+export default async function CoursePage() {
+  const response = await fetch('https://api.codingthailand.com/api/course');
+  const courseResponse = await response.json();
+
   return (
     <main>
+      {
+        JSON.stringify(courseResponse)
+      }
       <FeaturesCourse />
     </main>
   );
