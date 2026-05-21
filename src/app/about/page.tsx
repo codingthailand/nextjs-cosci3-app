117da707a740c9a3d7e83362c6d09bde62d76110
Akenarin Komkoon <codingthaialnd@gmail.com>
2026-05-21 10:30:59 +0700

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 import AppLoading from "../components/app-loading";
+import { Suspense } from "react";
+
+async function ApiVersion() {
+  const response = await fetch('https://api.codingthailand.com/api/version');
+  const apiInfo = await response.json();
+
+  return <p>API Version: {apiInfo.data.version}</p>;
+}
 
 // http://localhost:3000/about
 export default function AboutPage() {
   return (
     <main>
-      About Page
-      <AppLoading />
+      <Suspense fallback={ <AppLoading /> }>
+        <ApiVersion />
+      </Suspense>     
       <hr />
       <Link href="/" className="underline">
         Home Page
